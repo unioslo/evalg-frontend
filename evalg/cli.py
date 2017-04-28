3de26e04b764220746ef431ed9e8ecccd117009e
Jonas Braathen <jonas.braathen@usit.uio.no>
2017-04-28 11:37:50 +0200

    Add save() convenience method to shell context

--- evalg/cli.py
+++ evalg/cli.py
@@ -3,11 +3,19 @@
 """ eValg command line interface. """
 
 
+def save_object(obj):
+    from evalg import db
+    db.session.add(obj)
+    db.session.commit()
+    print("Saved {}".format(obj))
+
+
 def shell_context():
     """ Shell context. """
     from evalg import db, models
     from pprint import pprint
     context = {
+        'save': save_object,
         'db': db,
         'Election': models.election.Election,
         'ElectionGroup': models.election.ElectionGroup,
