905f68b4696447300783d29677cec4424ab0083f
Jo Sama <jo.sama@usit.uio.no>
2017-05-11 11:51:13 +0200

    Proper response codes for Candidate API.

--- evalg/api/candidate.py
+++ evalg/api/candidate.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """ The candidate API. """
-from flask import Blueprint, request, jsonify, abort, current_app
+from flask import Blueprint, abort, make_response
 from flask_apispec.views import MethodResource
 from flask_apispec import use_kwargs, marshal_with
 from flask_apispec import doc
@@ -34,6 +34,15 @@ class CandidateSchema(BaseSchema):
 candidate_schema = CandidateSchema()
 
 
+def get_candidate(id):
+    """ Get a candidate from the database. """
+    c = Candidate.query.get(id)
+    if c is None or c.hidden:
+        abort(404)
+    else:
+        return c
+
+
 @doc(tags=['candidate'])
 class CandidateList(MethodResource):
     """ Candidate API. """
@@ -62,13 +71,13 @@ class CandidateDetail(MethodResource):
     @doc(summary='Get a candidate')
     def get(self, id):
         """ List candidates. """
-        return Candidate.query.get(id)
+        return get_candidate(id)
 
     @marshal_with(CandidateSchema())
     @use_kwargs(CandidateSchema())
     @doc(summary='Partially update a candidate')
     def patch(self, id, **kwargs):
-        c = Candidate.query.get(id)
+        c = get_candidate(id)
         for k, v in kwargs.items():
             setattr(c, k, v)
         db.session.commit()
@@ -79,10 +88,10 @@ class CandidateDetail(MethodResource):
                 locations='query')
     @doc(summary='Delete a candidate')
     def delete(self, id):
-        l = Candidate.query.get(id)
-        l.hidden = True
+        c = get_candidate(id)
+        c.hidden = True
         db.session.commit()
-        return None, 204
+        return make_response('', 204)
 
 
 bp.add_url_rule('/candidates/',
