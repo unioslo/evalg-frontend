663e6f50859bffbc8d7e546856697a143ddb0c4a
Håkon Salomonsen Møller <hmo@hamok.uio.no>
2019-01-03 10:16:56 +0100

    Use census select page on mobile

--- src/routes/app/voter/frontpage/components/VoterElectionsList.js
+++ src/routes/app/voter/frontpage/components/VoterElectionsList.js
@@ -58,7 +58,7 @@ const VoterElectionsListItem = (props: ListItemProps) => {
         </div>
       </div>
       <ButtonContainer alignLeft>
-        <Link to={`/voter/elections/${election.id}/vote`}>
+        <Link to={`/voter/${electionGroup.id}/select-census`}>
           {!hasVoted ?
             <Button
               text={<Trans>election.voteNow</Trans>}
