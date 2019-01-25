ad3924b2d15bd2c193d09b72ea39cb5b78952dc0
Håkon Salomonsen Møller <hmo@hamok.uio.no>
2019-01-25 12:16:44 +0100

    Bugfix - don't return before callback is finnished, wrap in promise instead

--- src/cryptoEngines/nacl.js
+++ src/cryptoEngines/nacl.js
@@ -3,13 +3,15 @@
 import nacl_factory from 'js-nacl/lib/nacl_factory.js';
 
 const generateKeyPair = () => {
-  const keyPair = {};
-  nacl_factory.instantiate(nacl => {
-    const keys = nacl.crypto_box_keypair();
-    keyPair.publicKey = nacl.to_hex(keys.boxPk);
-    keyPair.secretKey = nacl.to_hex(keys.boxSk)
+  return new Promise(resolve => {
+    nacl_factory.instantiate(nacl => {
+      const keyPair = {};
+      const keys = nacl.crypto_box_keypair();
+      keyPair.publicKey = nacl.to_hex(keys.boxPk);
+      keyPair.secretKey = nacl.to_hex(keys.boxSk)
+      resolve(keyPair);
+    });
   });
-  return keyPair;
 };
 
 export const naCl = {
