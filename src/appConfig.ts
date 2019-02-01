aa27b4192acfe6065a5a167a5f6663ac68e00259
Jonas Braathen <jonas.braathen@usit.uio.no>
2019-02-01 17:52:11 +0100

    Ask for more scopes when authenticating

--- src/appConfig.ts
+++ src/appConfig.ts
@@ -5,12 +5,14 @@ export const appTimezone = "Europe/Oslo";
 /* Enable or dissable a simple test warning message */
 export const testWarning = false;
 
+export const feideGatekeeperScope = 'gk_evalg-something';
+
 export const oidcConfig = {
   authority: 'https://auth.dataporten.no/',
   client_id: '48f63358-9eee-429f-bd79-381fb12fbf6b',
   redirect_uri: 'http://localhost:3000/callback',
   response_type: 'id_token token',
-  scope: 'openid',
+  scope: feideGatekeeperScope + ' openid profile email groups userid userid-feide userinfo-entitlement',
 };
 export const oidcLogoutUrl = 'https://auth.dataporten.no/logout';
 
