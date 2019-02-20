d27a16df07883d7e9828d9d20e58c8bb516a3ab7
Jonas Braathen <jonas.braathen@usit.uio.no>
2019-02-20 19:27:27 +0100

    Inject environment variables into the window for runtime configuration

--- src/appConfig.ts
+++ src/appConfig.ts
@@ -1,25 +1,36 @@
+declare global {
+  /* tslint:disable */
+  interface Window {
+    ENV: any;
+  }
+}
+
+/* Locate the client environment */
+const isProduction = (process.env.NODE_ENV === 'production')
+const env = isProduction ? window.ENV : process.env;
+
 /* General settings */
 export const cryptoVariant: string = 'nacl';
 export const appTimezone: string = 'Europe/Oslo';
 
 /* Backends */
-export const graphqlBackend: string = process.env.REACT_APP_BACKEND_GRAPHQL as string;
-export const restBackend: string = process.env.REACT_APP_BACKEND_REST as string;
+export const graphqlBackend: string = env.REACT_APP_BACKEND_GRAPHQL as string;
+export const restBackend: string = env.REACT_APP_BACKEND_REST as string;
 
 /* Authentication */
-export const authEnabled: boolean = (process.env.REACT_APP_AUTH_ENABLED === 'true');
-export const feideGatekeeperScope: string = process.env.REACT_APP_FEIDE_GK_API_SCOPE as string;
+export const authEnabled: boolean = (env.REACT_APP_AUTH_ENABLED === 'true');
+export const feideGatekeeperScope: string = env.REACT_APP_FEIDE_GK_API_SCOPE as string;
 export const oidcConfig: Oidc.OidcClientSettings = {
   authority: 'https://auth.dataporten.no/',
-  client_id: process.env.REACT_APP_FEIDE_DP_APP_ID as string,
-  redirect_uri: process.env.REACT_APP_FEIDE_DP_REDIRECT_URI as string,
+  client_id: env.REACT_APP_FEIDE_DP_APP_ID as string,
+  redirect_uri: env.REACT_APP_FEIDE_DP_REDIRECT_URI as string,
   response_type: 'id_token token',
   scope: feideGatekeeperScope + ' openid profile email groups userid userid-feide userinfo-entitlement',
 };
 export const oidcLogoutUrl = 'https://auth.dataporten.no/logout';
 
 /* Show warning in the footer about this being a staging/test system */
-export const appStagingWarning: boolean = (process.env.REACT_APP_STAGING_WARNING === 'true');
+export const appStagingWarning: boolean = (env.REACT_APP_STAGING_WARNING === 'true');
 
 /* Footer content */
 export const appHelpLink: string = 'https://www.uio.no/for-ansatte/arbeidsstotte/arrangere-valg/';
