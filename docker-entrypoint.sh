7d1f610d4efa3dabb587199a97f79a5afc2c7dcf
Jonas Braathen <jonas.braathen@usit.uio.no>
2019-02-22 15:26:47 +0100

    Fix client environment path in docker entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,6 @@
 set -eu
 
 ENV_JSON=$(env | grep '^REACT_APP_*' | jq -c '. | split("\n") | map(select(. != "")) | map(split("=") | { (.[0]) : .[1] }) | reduce .[] as $item ({}; . * $item)' -R -s)
-echo "window.ENV = $ENV_JSON" > /app/env.js
+echo "window.ENV = $ENV_JSON" > /app/build/env.js
 
 exec "$@"
