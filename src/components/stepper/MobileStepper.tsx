94684c0faff9500e07556b316874ab9fb6855907
Håkon Salomonsen Møller <hmo@hamok.uio.no>
2019-03-06 19:20:44 +0100

    Voting stepper for mobile

---
+++ src/components/stepper/MobileStepper.tsx
@@ -0,0 +1,175 @@
+import * as React from 'react';
+import injectSheet from 'react-jss';
+import { Classes } from 'jss';
+import classNames from 'classnames';
+
+const styles = (theme: any) => ({
+  mobileStepper: {
+    width: '100%',
+    marginBottom: '2rem',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    '& svg': {
+      width: '100%',
+      height: 56,
+      '& rect': {
+        width: '100%',
+      },
+    },
+  },
+  rectangle: {
+    fill: theme.stepperColor,
+  },
+  sectionNumber: {
+    fontSize: '2.4rem',
+    fontWeight: 'bold',
+  },
+  rightCirclesSvg: {
+    overflow: 'visible',
+  },
+  stepText: {
+    fontSize: '1.8rem',
+  },
+  sectionNumberActive: {
+    fill: theme.stepperSectionTextColorActive,
+  },
+  sectionNumberInactive: {
+    fill: theme.stepperSectionTextColorInactive,
+  },
+  sectionCircleActive: {
+    fill: theme.stepperSectionCircleColorActive,
+  },
+  sectionCircleInactive: {
+    fill: theme.stepperSectionCircleColorInactive,
+  },
+});
+
+interface IProps {
+  numberOfSteps: number;
+  currentStepNumber: number;
+  stepText: string;
+  nextStepsToTheRight?: boolean;
+  title?: string;
+  classes: Classes;
+}
+
+const MobileStepper: React.SFC<IProps> = props => {
+  const {
+    numberOfSteps,
+    currentStepNumber,
+    stepText,
+    nextStepsToTheRight,
+    title,
+    classes,
+  } = props;
+
+  let stepNumbers = [];
+  for (let i = 1; i <= numberOfSteps; i++) {
+    stepNumbers.push(i);
+  }
+  const leftStepNumbers = stepNumbers.slice(0, currentStepNumber);
+  const rightStepNumbers = stepNumbers.slice(
+    currentStepNumber,
+    stepNumbers.length
+  );
+
+  const topPadding = 3;
+  const firstCircleXOffset = 4;
+  const circleRadius = 25;
+  const circleDiameter = 50;
+  const nextCircleXOffset = 39;
+  const numberXPos = 18;
+  const stepTextLeftPad = 15;
+
+  const stepTextXOffset =
+    firstCircleXOffset +
+    circleDiameter +
+    nextCircleXOffset * (currentStepNumber - 1) +
+    stepTextLeftPad;
+
+  return (
+    <div className={classes.mobileStepper}>
+      <svg>
+        {title && <title>{title}</title>}
+        <rect className={classes.rectangle} x="0" y="0" height="56" rx="28" />
+        {leftStepNumbers.map((stepNumber, index) => {
+          const circleXOffset =
+            firstCircleXOffset + circleRadius + nextCircleXOffset * index;
+
+          const numberXOffset =
+            firstCircleXOffset + numberXPos + nextCircleXOffset * index;
+
+          const numberClassNames = classNames({
+            [classes.sectionNumber]: true,
+            [classes.sectionNumberActive]: stepNumber === currentStepNumber,
+            [classes.sectionNumberInactive]: stepNumber !== currentStepNumber,
+          });
+
+          return (
+            <React.Fragment key={index}>
+              <circle
+                className={
+                  stepNumber === currentStepNumber
+                    ? classes.sectionCircleActive
+                    : classes.sectionCircleInactive
+                }
+                cx={circleXOffset}
+                cy={topPadding + circleRadius}
+                r={circleRadius}
+              />
+              <text className={numberClassNames}>
+                <tspan x={numberXOffset} y="37">
+                  {stepNumber}
+                </tspan>
+              </text>
+            </React.Fragment>
+          );
+        })}
+        {nextStepsToTheRight && (
+          <svg className={classes.rightCirclesSvg} x="100%" y={topPadding}>
+            {rightStepNumbers.map((stepNumber, index) => {
+              const circleXNegativeOffset =
+                firstCircleXOffset +
+                circleRadius +
+                nextCircleXOffset * (rightStepNumbers.length - 1 - index);
+
+              const numberXNegativeOffset =
+                firstCircleXOffset +
+                (circleDiameter - numberXPos) +
+                nextCircleXOffset * (rightStepNumbers.length - 1 - index);
+
+              const numberClassNames = classNames({
+                [classes.sectionNumber]: true,
+                [classes.sectionNumberInactive]: true,
+              });
+
+              return (
+                <React.Fragment key={index}>
+                  <circle
+                    className={classes.sectionCircleInactive}
+                    cx={-circleXNegativeOffset}
+                    cy={circleRadius}
+                    r={circleRadius}
+                  />
+                  <text className={numberClassNames}>
+                    <tspan x={-numberXNegativeOffset} y="34">
+                      {stepNumber}
+                    </tspan>
+                  </text>
+                </React.Fragment>
+              );
+            })}
+          </svg>
+        )}
+
+        <text className={classes.stepText}>
+          <tspan x={stepTextXOffset} y="35">
+            {stepText}
+          </tspan>
+        </text>
+      </svg>
+    </div>
+  );
+};
+
+export default injectSheet(styles)(MobileStepper);
