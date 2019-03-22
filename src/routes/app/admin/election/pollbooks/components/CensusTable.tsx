024709ae25c2daae54cb64bf1c368bb435f762ec
Håkon Salomonsen Møller <hmo@hamok.uio.no>
2019-03-22 14:19:33 +0100

    Own state for showing new voter form

--- src/routes/app/admin/election/pollbooks/components/CensusTable.tsx
+++ src/routes/app/admin/election/pollbooks/components/CensusTable.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import gql from 'graphql-tag';
 import { Mutation } from 'react-apollo';
 import { Trans, TranslationFunction } from 'react-i18next';
@@ -106,27 +106,40 @@ const CensusTable: React.FunctionComponent<IProps> = ({
   t,
   lang,
 }) => {
-  const [updateVoterId, setUpdateVoterId] = useState('');
-  const [deleteVoterId, setDeleteVoterId] = useState('');
+  const [showNewVoterForm, setShowNewVoterForm] = useState(false);
+  const [voterToUpdateId, setVoterToUpdateId] = useState('');
+  const [voterToDelete, setVoterToDelete] = useState<null | IVoter>(null);
   const [nRecordsToShow, setNRecordsToShow] = useState(
     N_RECORDS_TO_SHOW_INCREMENT
   );
 
-  const handleShowUpdateVoterFormForVoterId = (voterId: string) => {
+  useEffect(() => {
+    if (addVoterPollbookId) {
+      handleCloseUpdateVoterForm();
+      setShowNewVoterForm(true);
+    }
+  }, [addVoterPollbookId]);
+
+  const handleCloseAddVoterForm = () => {
+    setShowNewVoterForm(false);
     onCloseAddVoterForm();
-    setUpdateVoterId(voterId);
+  };
+
+  const handleShowUpdateVoterFormForVoterId = (voterId: string) => {
+    handleCloseAddVoterForm();
+    setVoterToUpdateId(voterId);
   };
 
   const handleCloseUpdateVoterForm = () => {
-    setUpdateVoterId('');
+    setVoterToUpdateId('');
   };
 
-  const handleShowDeleteVoterModalForVoterId = (voterId: string) => {
-    setDeleteVoterId(voterId);
+  const handleShowDeleteVoterModalForVoter = (voter: IVoter) => {
+    setVoterToDelete(voter);
   };
 
   const handleHideDeleteVoterModal = () => {
-    setDeleteVoterId('');
+    setVoterToDelete(null);
   };
 
   const handleIncreaseNRecordsToShow = () => {
@@ -160,160 +173,158 @@ const CensusTable: React.FunctionComponent<IProps> = ({
   );
 
   return (
-    <Table>
-      <TableHeader>
-        <TableHeaderRow>
-          <TableHeaderCell>
-            <Trans>census.idType</Trans>
-          </TableHeaderCell>
-          <TableHeaderCell>
-            <Trans>census.idValue</Trans>
-          </TableHeaderCell>
-          <TableHeaderCell>
-            <Trans>census.group</Trans>
-          </TableHeaderCell>
-          <TableHeaderCell />
-        </TableHeaderRow>
-      </TableHeader>
-      <TableBody>
-        {!!addVoterPollbookId && (
-          <AddVoterForm
-            pollbook={pollBookDict[addVoterPollbookId]}
-            onClose={onCloseAddVoterForm}
-            t={t}
-            lang={lang}
-          />
-        )}
+    <>
+      <Table>
+        <TableHeader>
+          <TableHeaderRow>
+            <TableHeaderCell>
+              <Trans>census.idType</Trans>
+            </TableHeaderCell>
+            <TableHeaderCell>
+              <Trans>census.idValue</Trans>
+            </TableHeaderCell>
+            <TableHeaderCell>
+              <Trans>census.group</Trans>
+            </TableHeaderCell>
+            <TableHeaderCell />
+          </TableHeaderRow>
+        </TableHeader>
+        <TableBody>
+          {showNewVoterForm && !!addVoterPollbookId && (
+            <AddVoterForm
+              pollbook={pollBookDict[addVoterPollbookId]}
+              onClose={handleCloseAddVoterForm}
+              t={t}
+              lang={lang}
+            />
+          )}
 
-        {filteredVotersToShow.map(voter => {
-          if (voter.id === updateVoterId && !addVoterPollbookId) {
-            return (
-              <React.Fragment key={voter.id}>
-                <TableRow verticalPadding={true}>
-                  <TableCell topPadding verticalAlignTop>
+          {filteredVotersToShow.map(voter => {
+            if (voter.id === voterToUpdateId) {
+              return (
+                <React.Fragment key={voter.id}>
+                  <TableRow verticalPadding={true}>
+                    <TableCell topPadding verticalAlignTop>
+                      <Text>{getVoterIdTypeDisplayName(voter.idType, t)}</Text>
+                    </TableCell>
+                    <TableCell topPadding verticalAlignTop>
+                      <Text>{voter.idValue}</Text>
+                    </TableCell>
+                    <TableCell colspan={2}>
+                      <Mutation
+                        mutation={updateVoterPollBook}
+                        refetchQueries={refetchQueries}
+                      >
+                        {updateVoter => {
+                          const update = (values: {
+                            id: string;
+                            pollbookId: string;
+                          }) => {
+                            updateVoter({ variables: values });
+                            handleCloseUpdateVoterForm();
+                          };
+                          return (
+                            <UpdateVoterForm
+                              initialValues={{
+                                pollbookId: voter.pollbookId,
+                                id: voter.id,
+                              }}
+                              submitAction={update}
+                              closeAction={handleCloseUpdateVoterForm}
+                              options={pollBookOptions}
+                              deleteAction={() =>
+                                handleShowDeleteVoterModalForVoter(voter)
+                              }
+                            />
+                          );
+                        }}
+                      </Mutation>
+                    </TableCell>
+                  </TableRow>
+                </React.Fragment>
+              );
+            } else {
+              return (
+                <TableRow key={voter.id} actionTextOnHover>
+                  <TableCell>
                     <Text>{getVoterIdTypeDisplayName(voter.idType, t)}</Text>
                   </TableCell>
-                  <TableCell topPadding verticalAlignTop>
+                  <TableCell>
                     <Text>{voter.idValue}</Text>
                   </TableCell>
-                  <TableCell colspan={2}>
-                    <Mutation
-                      mutation={updateVoterPollBook}
-                      refetchQueries={refetchQueries}
-                    >
-                      {updateVoter => {
-                        const update = (values: {
-                          id: string;
-                          pollbookId: string;
-                        }) => {
-                          updateVoter({ variables: values });
-                          handleCloseUpdateVoterForm();
-                        };
-                        return (
-                          <UpdateVoterForm
-                            initialValues={{
-                              pollbookId: voter.pollbookId,
-                              id: voter.id,
-                            }}
-                            submitAction={update}
-                            closeAction={handleCloseUpdateVoterForm}
-                            options={pollBookOptions}
-                            deleteAction={() =>
-                              handleShowDeleteVoterModalForVoterId(voter.id)
-                            }
-                          />
-                        );
-                      }}
-                    </Mutation>
+                  <TableCell>
+                    <Text>{pollBookDict[voter.pollbookId].name[lang]}</Text>
+                  </TableCell>
+                  <TableCell alignRight>
+                    <Text>
+                      <ActionText
+                        action={() =>
+                          handleShowUpdateVoterFormForVoterId(voter.id)
+                        }
+                      >
+                        <Trans>general.edit</Trans>
+                      </ActionText>
+                    </Text>
                   </TableCell>
                 </TableRow>
-                {!!deleteVoterId && (
-                  <Mutation
-                    mutation={deleteVoter}
-                    refetchQueries={refetchQueries}
-                  >
-                    {del => {
-                      const deleteAndClose = () => {
-                        del({ variables: { id: voter.id } });
-                        handleHideDeleteVoterModal();
-                      };
-                      return (
-                        <ConfirmModal
-                          confirmAction={deleteAndClose}
-                          closeAction={handleHideDeleteVoterModal}
-                          header={
-                            <Trans>
-                              census.deletePersonConfirmationModalTitle
-                            </Trans>
-                          }
-                          body={
-                            <Trans
-                              values={{
-                                idType: getVoterIdTypeDisplayName(
-                                  voter.idType,
-                                  t
-                                ).toLowerCase(),
-                                idValue: voter.idValue,
-                                pollbookName:
-                                  pollBookDict[voter.pollbookId].name[lang],
-                              }}
-                            >
-                              census.deletePersonConfirmationModalText
-                            </Trans>
-                          }
-                          confirmText={<Trans>general.delete</Trans>}
-                          closeText={<Trans>general.cancel</Trans>}
-                        />
-                      );
-                    }}
-                  </Mutation>
-                )}
-              </React.Fragment>
-            );
-          } else {
+              );
+            }
+          })}
+          <TableRow>
+            <TableCell colspan={4}>
+              {t('census.showingNOfNRecords', {
+                nRecordsShowing,
+                nRecordsTotal: filteredVoters.length,
+              })}{' '}
+              {nMoreRecordsToShowOnNextIncrease > 0 && (
+                <ActionText bottom action={handleIncreaseNRecordsToShow}>
+                  {t('census.showMoreRecordsPrompt', {
+                    nMoreRecordsToShow: nMoreRecordsToShowOnNextIncrease,
+                  })}
+                </ActionText>
+              )}
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+
+      {voterToDelete && (
+        <Mutation mutation={deleteVoter} refetchQueries={refetchQueries}>
+          {del => {
+            const deleteAndClose = () => {
+              del({ variables: { id: voterToDelete.id } });
+              handleHideDeleteVoterModal();
+            };
             return (
-              <TableRow key={voter.id} actionTextOnHover>
-                <TableCell>
-                  <Text>{getVoterIdTypeDisplayName(voter.idType, t)}</Text>
-                </TableCell>
-                <TableCell>
-                  <Text>{voter.idValue}</Text>
-                </TableCell>
-                <TableCell>
-                  <Text>{pollBookDict[voter.pollbookId].name[lang]}</Text>
-                </TableCell>
-                <TableCell alignRight>
-                  <Text>
-                    <ActionText
-                      action={() =>
-                        handleShowUpdateVoterFormForVoterId(voter.id)
-                      }
-                    >
-                      <Trans>general.edit</Trans>
-                    </ActionText>
-                  </Text>
-                </TableCell>
-              </TableRow>
+              <ConfirmModal
+                confirmAction={deleteAndClose}
+                closeAction={handleHideDeleteVoterModal}
+                header={
+                  <Trans>census.deletePersonConfirmationModalTitle</Trans>
+                }
+                body={
+                  <Trans
+                    values={{
+                      idType: getVoterIdTypeDisplayName(
+                        voterToDelete.idType,
+                        t
+                      ).toLowerCase(),
+                      idValue: voterToDelete.idValue,
+                      pollbookName:
+                        pollBookDict[voterToDelete.pollbookId].name[lang],
+                    }}
+                  >
+                    census.deletePersonConfirmationModalText
+                  </Trans>
+                }
+                confirmText={<Trans>general.delete</Trans>}
+                closeText={<Trans>general.cancel</Trans>}
+              />
             );
-          }
-        })}
-        <TableRow>
-          <TableCell colspan={4}>
-            {t('census.showingNOfNRecords', {
-              nRecordsShowing,
-              nRecordsTotal: filteredVoters.length,
-            })}{' '}
-            {nMoreRecordsToShowOnNextIncrease > 0 && (
-              <ActionText bottom action={handleIncreaseNRecordsToShow}>
-                {t('census.showMoreRecordsPrompt', {
-                  nMoreRecordsToShow: nMoreRecordsToShowOnNextIncrease,
-                })}
-              </ActionText>
-            )}
-          </TableCell>
-        </TableRow>
-      </TableBody>
-    </Table>
+          }}
+        </Mutation>
+      )}
+    </>
   );
 };
 
