b50ec4615c450dc836079232d7da09ac88cfb046
Jo Sama <jo.sama@usit.uio.no>
2019-04-02 14:39:12 +0200

    Configure storybook
    Adds the basic configuration, and a decorator that wraps components that
    are to be storybooked with appropriate providers.

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,26 @@
-import { configure } from '@storybook/react';
+import { configure, addDecorator } from '@storybook/react';
+
+import React from 'react';
+import { ThemeProvider } from 'react-jss';
+import theme from '../src/theme';
+import { BrowserRouter } from 'react-router-dom';
+
+import '../src/i18n';
+import Spinner from '../src/components/animations/Spinner';
+
+const pc = fn => (
+  <BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <React.Suspense fallback={<Spinner />}>{fn()}</React.Suspense>
+    </ThemeProvider>
+  </BrowserRouter>
+);
+addDecorator(pc);
+
+const req = require.context('../src/stories', true, /\.stories\.*$/);
 
 function loadStories() {
-  require('../src/stories');
+  req.keys().forEach(filename => req(filename));
 }
 
 configure(loadStories, module);
