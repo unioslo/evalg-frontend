e373a5e5ff272255ad91266468140bf7ab7601bc
Jonas Braathen <jonas.braathen@usit.uio.no>
2019-04-08 15:58:28 +0200

    Convert component DropdownArrowIcon to TypeScript

--- src/components/icons/DropdownArrowIcon.tsx
+++ src/components/icons/DropdownArrowIcon.tsx
@@ -1,25 +1,26 @@
 import * as React from 'react';
 import injectSheet from 'react-jss';
+import { Classes } from 'jss';
 
-// type Props = {
-//   selected?: boolean,
-//   action: Function,
-//   classes: Object
-// }
+interface IProps {
+  selected?: boolean;
+  action?: () => void;
+  classes: Classes;
+}
 
-const styles = theme => ({
+const styles = (theme: any) => ({
   arrow: {
     display: 'inline-block',
     '&:hover': {
-      cursor: 'pointer'
-    }
+      cursor: 'pointer',
+    },
   },
   arrowIcon: {
-    fill: theme.dropDownArrowColor
-  }
-})
+    fill: theme.dropDownArrowColor,
+  },
+});
 
-const TableCellDropdownArrow = (props) => {
+const DrowdownArrowIcon = (props: IProps) => {
   let transform = 'translate(0, 2)';
   if (props.selected) {
     transform = 'translate(-5, 0) rotate(90, 6, 12)';
@@ -27,13 +28,15 @@ const TableCellDropdownArrow = (props) => {
   return (
     <div onClick={props.action} className={props.classes.arrow}>
       <svg width="30" height="20" viewBox="0 0 19 20">
-        <path d="M2.609 0L0 2.608l6.506 6.604L0 15.715l2.609 2.609 9.112-9.112z"
+        <path
+          d="M2.609 0L0 2.608l6.506 6.604L0 15.715l2.609 2.609 9.112-9.112z"
           className={props.classes.arrowIcon}
           transform={transform}
-          fillRule="evenodd" />
+          fillRule="evenodd"
+        />
       </svg>
     </div>
-  )
+  );
 };
 
-export default injectSheet(styles)(TableCellDropdownArrow);
+export default injectSheet(styles)(DrowdownArrowIcon);
