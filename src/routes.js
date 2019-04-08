b68064158537f9bedf09af5c97fec59a5a4f5dd2
Jonas Braathen <jonas.braathen@usit.uio.no>
2019-04-08 15:48:14 +0200

    Remove unused file

--- src/routes.js
+++
@@ -1,7 +0,0 @@
-export const basePath = '/';
-export const voterPath = 'voter';
-export const adminPath = 'admin';
-export const infoPath = 'info';
-export const candidatesPath = 'candidates';
-export const censusesPath = 'censuses';
-export const statusPath = 'status';
