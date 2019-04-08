c9df3c1b3aabe5edc046dcc2f76eba74b312a1d9
Jonas Braathen <jonas.braathen@usit.uio.no>
2019-04-08 15:03:21 +0200

    Convert a few minor components related to the candidate table to TypeScript

---
+++ src/routes/app/admin/election/candidates/components/TableRowWithText.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+
+import Text from '../../../../../../components/text';
+import { TableRow, TableCell } from '../../../../../../components/table';
+
+interface IProps {
+  colSpan: number;
+  children?: React.ReactNode;
+}
+
+const TableRowWithText: React.FunctionComponent<IProps> = (props: IProps) => {
+  return (
+    <TableRow>
+      <TableCell colspan={props.colSpan}>
+        <Text>{props.children}</Text>
+      </TableCell>
+    </TableRow>
+  );
+};
+
+export default TableRowWithText;
