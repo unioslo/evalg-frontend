9840a35f7dd1442cf666c7ecca61b599657d9916
Marius Jean Bischof Hagalid <marius.hagalid@usit.uio.no>
2019-05-09 10:30:07 +0200

    Address review comments
    EVALG-537-verify-vote

--- src/routes/app/voter/vote/MajorityVote.tsx
+++ src/routes/app/voter/vote/MajorityVote.tsx
@@ -98,9 +98,9 @@ class MajorityVote extends React.Component<
     this.props.onSubmitVote({
       voteType: 'majorityVote',
       isBlankVote: this.state.isBlankVote,
-      rankedCandidateIds: this.state.isBlankVote
-        ? []
-        : this.state.selectedCandidate && [this.state.selectedCandidate.id],
+      rankedCandidateIds: this.state.selectedCandidate
+        ? [this.state.selectedCandidate.id]
+        : [],
     });
   };
 }
