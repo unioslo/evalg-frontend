1431ed75325dd6dd2b51b402406c8263d0265de5
Sivert Hatteberg <sivert.hatteberg@usit.uio.no>
2020-06-19 15:03:06 +0200

    Api changes in the i18next package
    Issue: EVALG-744

--- src/i18n.ts
+++ src/i18n.ts
@@ -13,7 +13,7 @@ i18n
       'en-US': ['en'],
       default: ['nb'],
     },
-    whitelist: ['nb', 'en', 'nn'],
+    supportedLngs: ['nb', 'en', 'nn'],
     debug: process.env.NODE_ENV !== 'production',
 
     backend: {
