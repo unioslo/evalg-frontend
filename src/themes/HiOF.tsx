a35f1e46d9e8c711bb91a130740ef69ab65cf4f9
Stein Elgethun <stein.elgethun@usit.uio.no>
2021-06-29 12:38:18 +0200

    Add HiOF theme
    Issue: EVALG-951

---
+++ src/themes/HiOF.tsx
@@ -0,0 +1,18 @@
+
+const white = '#FFFFFF';
+const black = '#23201F';
+
+const aquaBlue = '#347E84';
+const lightWarmGray = '#ECEAE5';
+
+const HiOFTheme = {
+  headerMainAreaColor: aquaBlue,
+  headerLogoBarColor: aquaBlue,
+  headerDescColor: white,
+  footerBackgroundColor: lightWarmGray,
+  footerTextColor: black,
+  actionTextColor: aquaBlue,
+  loginPageBgColor: white,
+};
+
+export default HiOFTheme
