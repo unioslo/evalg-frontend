56e2bc407549061c1930a2cca864b357c78e1cb6
Sivert Hatteberg <sivert.hatteberg@usit.uio.no>
2021-08-11 21:09:46 +0200

    Remove storybook
    Storybook was used to any great extent. Remove to cleanup code and
    remove dependencies

--- src/components/button/PrimaryButton.stories.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { storiesOf, action } from '@kadira/storybook';
-import PrimaryButton from './PrimaryButton';
-
-storiesOf('form.button.Primarybutton', module).add('with text', () => (
-  <PrimaryButton action={action('clicked')} text="Noe tekst" />
-));
