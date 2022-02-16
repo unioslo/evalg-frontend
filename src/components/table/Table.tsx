d413d62822454d0ed4d98dce9e195a4ae513fc26
Sivert Hatteberg <sivert.hatteberg@usit.uio.no>
2022-02-16 23:06:29 +0100

    Fix a bug with Table, and remove a console warning from Spinner

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -2,29 +2,29 @@ import React from 'react';
 import classNames from 'classnames';
 import { createUseStyles } from 'react-jss';
 
-interface IProps {
+type TableProps = {
   marginTop?: string;
-}
+  children?: React.ReactNode;
+};
 
 const useStyles = createUseStyles({
   table: {
     width: '100%',
     userSelect: 'text',
-    marginTop: (props: IProps) => (props.marginTop ? props.marginTop : '4rem'),
+    marginTop: (props: TableProps) =>
+      props.marginTop ? props.marginTop : '4rem',
     '& th:not(:last-child), & td:not(:last-child)': {
       paddingRight: '2rem',
     },
   },
 });
 
-const Table: React.FunctionComponent<IProps> = (props) => {
+export default function Table(props: TableProps) {
   const { children } = props;
-  const classes = useStyles();
+  const classes = useStyles({ ...props });
 
   const cls = classNames({
     [classes.table]: true,
   });
   return <table className={cls}>{children}</table>;
-};
-
-export default Table;
+}
